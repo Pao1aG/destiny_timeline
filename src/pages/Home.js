6315ed014901834fb7cdd1d3500086ebd5e006da
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-09-24 12:50:21 -0700

    Reworked home page with buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,18 +7,18 @@ export default function Home() {
 
     return (
         <>
-            <h1> This is the Home Page</h1>
-            <div className="parallax-container">
-                <div className="parallax"><img src="images/parallax1.jpg"/></div>
-            </div>
-            <div className="section white">
-            <div className="row container">
-                <h2 className="header">Parallax</h2>
-                <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
-            </div>
-            </div>
-            <div className="parallax-container">
-                <div className="parallax"><img src="images/parallax2.jpg"/></div>
+            <div className="container">
+                <div className="row">
+                    <div className="col s12 m6 l7 text-container center-align">
+                        <p> Welcome! Let's tailor your experience...</p>
+                        <p> Are you a new or returning Guardian?</p>
+                        <img src="images/ghost_freezeframe.jpg"></img>
+                    </div>
+                    <div className="col s12 m6 l3 button-container">
+                        <a className="waves-effect waves-light btn-large">I'm a new Guardian</a>
+                        <a className="waves-effect waves-light btn-large">I'm a returning Guardian</a>
+                    </div>
+                </div>
             </div>
         </>
     )
