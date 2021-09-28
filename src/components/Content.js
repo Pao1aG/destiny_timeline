222c6f1876bd88c675ab3feeafc101c4780caee1
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-09-27 18:00:25 -0700

    Added Content component with reactive img/txt

---
+++ src/components/Content.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Content() {
+
+    return(
+        <div className="row timeline-main">
+            <p id="destiny-year"> Destiny 2 Year 1 </p>
+            <h1> TITLE </h1>
+            <div className="col s12 center-align">
+                <img src="images/destiny_wallpaper_1920x450.png" alt="" className="chapter-imgs responsive-img"/>
+            </div>
+            <div className="col s12 chapter-content">
+                <p className="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="weapon-imgs">
+                    <img src="http://placekitten.com/g/100/100" alt="" />
+                    <img src="http://placekitten.com/g/100/100" alt=""/>
+                    <img src="http://placekitten.com/g/100/100" alt=""/>
+                </div>
+                <p className="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+    )
+
+}
