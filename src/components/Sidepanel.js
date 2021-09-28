a0db91813c21e7fd9a37dd461df54f12e7d3067f
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-09-27 17:12:00 -0700

    Responsive sidenav icon for mobile

--- src/components/Sidepanel.js
+++ src/components/Sidepanel.js
@@ -3,12 +3,27 @@ import React from 'react';
 export default function Sidepanel () {
 
     return (
-        <div className="side-panel">
-            <img src="http://placekitten.com/g/200/200" alt="" className="responsive-img"/>
-            <img src="http://placekitten.com/g/200/200" alt="" className="responsive-img"/>
-            <img src="http://placekitten.com/g/200/200" alt="" className="responsive-img"/>
-            <img src="http://placekitten.com/g/200/200" alt="" className="responsive-img"/>
-            <img src="http://placekitten.com/g/200/200" alt="" className="responsive-img"/>
+        <div className="center-align side-panel">
+            <ul className="sidenav sidenav-fixed" id="slide-out">
+                <li> 
+                    <img src="http://placekitten.com/g/200/200" alt=""/>
+                </li>
+                <li> 
+                    <img src="http://placekitten.com/g/200/200" alt=""/>
+                </li>
+                <li> 
+                    <img src="http://placekitten.com/g/200/200" alt=""/>
+                </li>
+                <li> 
+                    <img src="http://placekitten.com/g/200/200" alt=""/>
+                </li>
+                <li> 
+                    <img src="http://placekitten.com/g/200/200" alt=""/>
+                </li>
+            </ul> 
+            <div id="icon-div">
+                <a href="/timeline" data-target="slide-out" className="sidenav-trigger" id="hide-icon"><i class="large material-icons left icon-white">chevron_right</i></a>    
+            </div>
         </div>
     )
 }
--- src/pages/TimelineSample.js
+++ src/pages/TimelineSample.js
@@ -4,27 +4,26 @@ import Sidepanel from '../components/Sidepanel';
 export default function TimelineSample() {
 
     return (
-        <div className="timeline-block">
-            {/* SIDE PANEL ICONS GO HERE */}
-            <Sidepanel />
-            {/* CONTENT GOES HERE */}
-            <div className="timeline-main">
-                <div className="center-align">
-                    <img src="images/parallax2.jpg" alt="" className="chapter-imgs"/>
+        <>
+        {/* SIDE PANEL ICONS GO INSIDE Sidepanel.js */}
+        <Sidepanel />
+        {/* CONTENT GOES HERE */}
+        <div className="timeline-main">
+            <div className="center-align container">
+                <img src="images/parallax2.jpg" alt="" className="chapter-imgs"/>
+            </div>
+            <div className="chapter-content container">
+                <p className="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="weapon-imgs">
+                    <img src="http://placekitten.com/g/100/100" alt="" />
+                    <img src="http://placekitten.com/g/100/100" alt=""/>
+                    <img src="http://placekitten.com/g/100/100" alt=""/>
                 </div>
-                <div className="chapter-content container">
-                    <p className="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p className="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <div className="weapon-imgs">
-                        <img src="http://placekitten.com/g/100/100" alt="" />
-                        <img src="http://placekitten.com/g/100/100" alt=""/>
-                        <img src="http://placekitten.com/g/100/100" alt=""/>
-                    </div>
-                    <p className="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p className="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
+                <p className="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
         </div>
-       
+       </>
     )
 }
