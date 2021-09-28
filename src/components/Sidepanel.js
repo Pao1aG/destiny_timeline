1c0996da0dd636bc7a274f3f43f7e0d49f08bb8f
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-09-27 20:34:13 -0700

    Test tooltip and separators for sidenav

--- src/components/Sidepanel.js
+++ src/components/Sidepanel.js
@@ -1,26 +1,69 @@
 import React from 'react';
 
 export default function Sidepanel () {
-    
+
 
     return (
         <div className="center-align side-panel">
             <ul className="sidenav sidenav-fixed" id="slide-out">
                 <li> 
-                    <img src="http://placekitten.com/g/200/200" alt=""/>
+                    <img src="http://placekitten.com/g/200/200" alt="" 
+                    className="btn tooltipped" 
+                    data-position="right" 
+                    data-tooltip="Meow" 
+                    id="btn-icons"/>
+                </li>
+                <li id="dots">
+                    <i class="medium material-icons icon-white">more_vert</i>
                 </li>
                 <li> 
-                    <img src="http://placekitten.com/g/200/200" alt=""/>
+                    <img src="http://placekitten.com/g/200/200" alt="" 
+                    className="btn tooltipped" 
+                    data-position="right" 
+                    data-tooltip="Meow" 
+                    id="btn-icons"/>
+                </li>
+                <li id="dots">
+                    <i class="medium material-icons icon-white">more_vert</i>
                 </li>
                 <li> 
-                    <img src="http://placekitten.com/g/200/200" alt=""/>
+                    <img src="http://placekitten.com/g/200/200" alt="" 
+                    className="btn tooltipped" 
+                    data-position="right" 
+                    data-tooltip="Meow" 
+                    id="btn-icons"/>
+                </li>
+                <li id="dots">
+                    <i class="medium material-icons icon-white">more_vert</i>
                 </li>
                 <li> 
-                    <img src="http://placekitten.com/g/200/200" alt=""/>
+                    <img src="http://placekitten.com/g/200/200" alt="" 
+                    className="btn tooltipped" 
+                    data-position="right" 
+                    data-tooltip="Meow" 
+                    id="btn-icons"/>
+                </li>
+                <li id="dots">
+                    <i class="medium material-icons icon-white">more_vert</i>
                 </li>
                 <li> 
-                    <img src="http://placekitten.com/g/200/200" alt=""/>
+                    <img src="http://placekitten.com/g/200/200" alt="" 
+                    className="btn tooltipped" 
+                    data-position="right" 
+                    data-tooltip="Meow" 
+                    id="btn-icons"/>
                 </li>
+                <li id="dots">
+                    <i class="medium material-icons icon-white">more_vert</i>
+                </li>
+                <li> 
+                    <img src="http://placekitten.com/g/200/200" alt="" 
+                    className="btn tooltipped" 
+                    data-position="right" 
+                    data-tooltip="Meow" 
+                    id="btn-icons"/>
+                </li>
+            
             </ul> 
             <div id="icon-div">
                 <a href="/timeline" data-target="slide-out" className="sidenav-trigger" id="hide-icon"><i class="large material-icons left icon-white">chevron_right</i></a>    
